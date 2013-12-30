e119dc325631d6873cdd972e6a529da3b061f842
Manu Goyal <manu.goyal2013@gmail.com>
2013-12-30 12:43:48 -0500

    Updated documentation in accordance with bacc399

--- dir_ac4ea4bd8ac261329328d8272b4742e6.js
+++ dir_ac4ea4bd8ac261329328d8272b4742e6.js
@@ -1,4 +1,4 @@
 var dir_ac4ea4bd8ac261329328d8272b4742e6 =
 [
-    [ "lib", "dir_27647d5548fdf53cd0fc0f7562a58277.html", "dir_27647d5548fdf53cd0fc0f7562a58277" ]
+    [ "libcuckoo", "dir_e4efb664af72a21142a7864d180da0f4.html", "dir_e4efb664af72a21142a7864d180da0f4" ]
 ];
